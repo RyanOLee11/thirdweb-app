2420760a616a11993cbee34d0c5919ac33bdc0b5
Ryan Lee <ryan.legit config --global user.name Your>
2023-01-28 13:24:52 -0500

    more changes gh pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
-import {BrowserRouter as Router } from 'react-router-dom';
+import {HashRouter as Router } from 'react-router-dom';
 import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
 import { StateContextProvider } from './context';
 
