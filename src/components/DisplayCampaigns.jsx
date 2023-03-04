b907f9814b76c41caec59727b6f448930ec5a623
Ryan Lee <ryan.legit config --global user.name Your>
2023-03-04 12:05:46 -0500

    removed unneccesary cards

--- src/components/DisplayCampaigns.jsx
+++ src/components/DisplayCampaigns.jsx
@@ -13,7 +13,7 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
   
   return (
     <div>
-      <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">{title} ({campaigns.length})</h1>
+      <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">{title} ({campaigns.length - 1})</h1>
 
       <div className="flex flex-wrap mt-[20px] gap-[26px]">
         {isLoading && (
@@ -26,11 +26,16 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
           </p>
         )}
 
-        {!isLoading && campaigns.length > 0 && campaigns.map((campaign) => <FundCard 
-          key={campaign.pId}
-          {...campaign}
-          handleClick={() => handleNavigate(campaign)}
-        />)}
+        {!isLoading && campaigns.length > 0 && campaigns.map((campaign) => {
+          if (campaign.title != 'arnav needs dick'){
+            return (<FundCard 
+              key={campaign.pId}
+              {...campaign}
+              handleClick={() => handleNavigate(campaign)}
+            />)
+          }
+        } 
+        )}
       </div>
     </div>
   )
